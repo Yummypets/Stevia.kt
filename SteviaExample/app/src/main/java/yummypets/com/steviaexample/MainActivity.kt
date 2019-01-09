526a804545338d375648fdd007d259817bdf1c4d
S4cha <sachadso@gmail.com>
2019-01-09 16:14:38 +0100

    Adds Empty example project

---
+++ SteviaExample/app/src/main/java/yummypets/com/steviaexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package yummypets.com.steviaexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
